27820117d805bf3acffb92185818249b6a7cde7f
pratibha <pratibhapaudel555@gmail.com>
2023-04-16 18:04:54 +1000

    modified

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  preset: "ts-jest",
+  // testEnvironment: 'jsdom',
+  // transform: {
+  //   '^.+\\.jsx?$': 'babel-jest',
+  //   '^.+\\.tsx?$': 'ts-jest',
+  // },
+  // moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // moduleNameMapper: {
+  //   '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  // },
+  // setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
